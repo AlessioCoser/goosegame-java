0df5af021b7896db2c68ce644eb9b9680651b130
Alessio Coser <>
2021-10-07 09:22:10 +0200

    first commit

---
+++ src/main/java/goosegame/Game.java
@@ -0,0 +1,5 @@
+package goosegame;
+
+public class Game {
+
+}
---
+++ src/test/java/goosegame/GameTest.java
@@ -0,0 +1,13 @@
+package goosegame;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class GameTest {
+    @Test
+    public void fixme() {
+        fail();
+    }
+}
+
